722c470490548a2022d5642c2fe75662d6c69157
thom-nguyen <thomasnguyen755@gmail.com>
2025-03-13 18:20:42 -0400

    chore: remove unused sync channels

--- src/components/channels/channel-content-modal.tsx
+++ src/components/channels/channel-content-modal.tsx
@@ -5,7 +5,6 @@ import useAppStore from "../../store";
 import { isNull } from "lodash";
 import ChannelContentPushMessage from "./sync/channel-content-push-message";
 import ChannelContentModeClock from "./sync/channel-content-mode-clock";
-import ChannelContentModeNba from "./sync/channel-content-mode-nba";
 
 function ChannelContentModal() {
   const { openChannel, setOpenChannel } = useAppStore();
@@ -47,8 +46,6 @@ function ChannelContentModal() {
               return <ChannelContentPushMessage />;
             case CHANNEL_ID_ENUM.SYNC_MODE_CLOCK:
               return <ChannelContentModeClock />;
-            case CHANNEL_ID_ENUM.SYNC_MODE_NBA:
-              return <ChannelContentModeNba />;
             default:
               return <>Placeholder content for channel ID: {openChannel.id}</>;
           }
--- src/components/channels/sync/channels.tsx
+++ src/components/channels/sync/channels.tsx
@@ -15,18 +15,4 @@ export const SyncChannels: (SyncChannel)[] = [
     icon: 'src\\assets\\clock.png',
     getSubheader: () => 'America/Toronto',
   },
-  {
-    id: CHANNEL_ID_ENUM.SYNC_MODE_NBA,
-    name: 'NBA Scoreboard (WIP)',
-    type: CHANNEL_TYPE_ENUM.SYNC_MODE,
-    icon: 'src\\assets\\nba.png',
-    getSubheader: () => "Today's NBA games",
-  },
-  {
-    id: CHANNEL_ID_ENUM.SYNC_MODE_SPOTIFY,
-    name: 'Spotify (WIP)',
-    type: CHANNEL_TYPE_ENUM.SYNC_MODE,
-    icon: 'src\\assets\\spotify.png',
-    getSubheader: () => "Show what's playing on Spotify",
-  },
 ];
